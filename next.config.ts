a8f044f054a0cb71d1d1c06af11a60d30df4c002
Jhony Pereira - Okea <jhony-16@live.com>
2026-03-03 13:47:25 -0300

    feat: Atualize os arquivos de configuração para implantação independente e adicione novas dependências.

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
    /* config options here */
+   output: 'standalone',
    allowedDevOrigins: [
       'localhost',
       '127.0.0.1',
-      '192.168.2.100', // IP da máquina na rede local
+      '192.168.2.100',
+      '192.168.0.102',
       'local-origin.dev',
       '*.local-origin.dev',
    ],
