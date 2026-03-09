7f32b55f0803ff59697e2e29bc3336d5af130f95
Jhony Pereira <jhony-16@live.com>
2026-03-08 22:57:30 -0300

    feat(dialog): adicionar suporte para tipo de botão destrutivo

--- components/dialog-confirmacao.tsx
+++ components/dialog-confirmacao.tsx
@@ -2,84 +2,84 @@
 import { Button } from './ui/button';
 import React, { useCallback } from 'react';
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
+   Dialog,
+   DialogContent,
+   DialogDescription,
+   DialogFooter,
+   DialogHeader,
+   DialogTitle,
 } from './ui/dialog';
 
 interface DialogConfirmacaoProps {
-  titulo: string;
-  textoPadrao: string;
-  dialogAberto: boolean;
-  setDialogAberto: (aberto: boolean) => void;
-  dialogLoading: boolean;
-  handleSubmit: () => Promise<void>;
-  btnCancelar?: string;
-  btnConfirmar?: string;
+   titulo: string;
+   textoPadrao: string | React.ReactNode;
+   dialogAberto: boolean;
+   setDialogAberto: (aberto: boolean) => void;
+   dialogLoading: boolean;
+   handleSubmit: () => Promise<void>;
+   btnCancelar?: string;
+   btnConfirmar?: string;
+   tipo: 'destrutivo' | 'padrao' | 'cancelamento';
 }
 
-const DialogConfirmacao: React.FC<
-  DialogConfirmacaoProps
-> = ({
-  titulo = 'Deseja continuar com o cancelamento?',
-  textoPadrao = 'Ao continuar você perderá todas as informações preenchidas, deseja continuar?',
-  btnCancelar = 'Cancelar',
-  btnConfirmar = 'Confirmar',
-  dialogAberto,
-  setDialogAberto,
-  dialogLoading,
-  handleSubmit,
+const DialogConfirmacao: React.FC<DialogConfirmacaoProps> = ({
+   titulo = 'Deseja continuar com o cancelamento?',
+   textoPadrao = 'Ao continuar você perderá todas as informações preenchidas, deseja continuar?',
+   btnCancelar = 'Cancelar',
+   btnConfirmar = 'Confirmar',
+   dialogAberto,
+   setDialogAberto,
+   dialogLoading,
+   handleSubmit,
+   tipo,
 }) => {
-  const handlePreventInteraction = useCallback(
-    (event: Event) => {
+   const handlePreventInteraction = useCallback((event: Event) => {
       event.preventDefault();
-    },
-    [],
-  );
-  return (
-    <Dialog
-      open={dialogAberto}
-      onOpenChange={setDialogAberto}
-    >
-      <DialogContent
-        onPointerDownOutside={handlePreventInteraction}
-        onInteractOutside={handlePreventInteraction}
-        onEscapeKeyDown={handlePreventInteraction}
-        onCloseAutoFocus={handlePreventInteraction}
-        className="border-border bg-popover text-popover-foreground max-w-11/12 rounded-lg border p-6 shadow-lg sm:max-w-xl [&_svg]:hidden"
-        role="dialog"
+   }, []);
+
+   return (
+      <Dialog
+         open={dialogAberto}
+         onOpenChange={setDialogAberto}
       >
-        <DialogHeader>
-          <DialogTitle className="text-xl font-semibold">
-            {titulo}
-          </DialogTitle>
-          <DialogDescription className="text-muted-foreground">
-            {textoPadrao}
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="gap-2">
-          <Button
-            variant="outline"
-            onClick={() => setDialogAberto(false)}
-            disabled={dialogLoading}
-            className=""
-          >
-            {btnCancelar}
-          </Button>
-          <Button
-            variant="destructive"
-            onClick={handleSubmit}
-            disabled={dialogLoading}
-          >
-            {btnConfirmar}
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+         <DialogContent
+            onPointerDownOutside={handlePreventInteraction}
+            onInteractOutside={handlePreventInteraction}
+            onEscapeKeyDown={handlePreventInteraction}
+            onCloseAutoFocus={handlePreventInteraction}
+            className="border-border bg-popover text-popover-foreground max-w-11/12 rounded-lg border p-6 shadow-lg sm:max-w-xl"
+            role="dialog"
+            showCloseButton={false}
+         >
+            <DialogHeader>
+               <DialogTitle className="text-xl font-semibold">
+                  {titulo}
+               </DialogTitle>
+               <DialogDescription className="text-muted-foreground">
+                  {textoPadrao}
+               </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="gap-2">
+               <Button
+                  variant="outline"
+                  onClick={() => setDialogAberto(false)}
+                  disabled={dialogLoading}
+                  className="uppercase"
+               >
+                  {btnCancelar}
+               </Button>
+               <Button
+                  variant={tipo === 'destrutivo' ? 'destructive' : 'default'}
+                  onClick={handleSubmit}
+                  disabled={dialogLoading}
+                  className="uppercase"
+               >
+                  {btnConfirmar}
+               </Button>
+            </DialogFooter>
+         </DialogContent>
+      </Dialog>
+   );
 };
 
 export default DialogConfirmacao;
